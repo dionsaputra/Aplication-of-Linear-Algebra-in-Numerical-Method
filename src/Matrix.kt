2855f32fe4beb9f30049a58c16c3bc84ea6dbb95
dionsaputra <dion.saputra@tiket.com>
2020-11-07 23:10:07 +0700

    implement matrix inverse

--- src/Matrix.kt
+++ src/Matrix.kt
@@ -1,3 +1,6 @@
+import kotlin.math.abs
+import kotlin.math.max
+
 data class Matrix(private var value: Array<DoubleArray>) {
 
     private val rows get() = value.size
@@ -13,7 +16,13 @@ data class Matrix(private var value: Array<DoubleArray>) {
         return value.contentDeepHashCode()
     }
 
+    override fun toString() = value.contentDeepToString()
+
     operator fun get(row: Int, col: Int) = value[row][col]
+
+    operator fun set(row: Int, col: Int, element: Double) {
+        value[row][col] = element
+    }
 
     operator fun plus(other: Matrix) = elementWise(other, Double::plus)
 
@@ -30,6 +39,62 @@ data class Matrix(private var value: Array<DoubleArray>) {
         return Matrix(result)
     }
 
+    fun det(): Double {
+        require(rows == cols)
+
+        var det = 0.0
+        for (j in 0 until cols) {
+            var incValue = 1.0
+            var decValue = 1.0
+            for (i in 0 until rows) {
+                incValue *= this[i, (j + i) % cols]
+                decValue *= this[rows - i - 1, (j + i) % cols]
+            }
+            det += (incValue - decValue)
+        }
+        return det
+    }
+
+    fun inverse(): Matrix? {
+        require(rows == cols)
+        if (det() == 0.0) return null
+
+        val result = Matrix.identity(rows)
+        val temp = clone()
+        for (fdRow in 0 until rows) {
+            // find focus-diagonal element (first non-zero element in i-th row)
+            var fdCol = 0
+            while (fdCol < cols && temp[fdRow, fdCol].equalsDelta(0.0)) fdCol++
+
+            // matrix hasn't inverse if all row is zero
+            if (fdCol == cols) return null
+
+            // scale current row so it's fd has value 1.0
+            val scalingFactor = 1/temp[fdRow, fdCol]
+            temp.rowScaling(fdRow, scalingFactor)
+            result.rowScaling(fdRow, scalingFactor)
+
+            // subtract other row with current-row * subs-factor
+            for (i in 0 until rows) {
+                if (i == fdRow) continue
+                val subFactor = temp[i, fdCol]
+                for (j in 0 until cols) {
+                    temp[i, j] -= subFactor * temp[fdRow, j]
+                    result[i, j] -= subFactor * result[fdRow, j]
+                }
+            }
+        }
+
+        return result
+    }
+
+    fun clone(): Matrix {
+        val value = Array(rows) { row ->
+            DoubleArray(cols) { col -> this[row, col] }
+        }
+        return Matrix(value)
+    }
+
     private fun elementWise(other: Matrix, transform: Double.(Double) -> Double): Matrix {
         require(cols == other.cols && rows == other.rows)
         val result = Array(rows) { DoubleArray(cols) }
@@ -39,7 +104,26 @@ data class Matrix(private var value: Array<DoubleArray>) {
         return Matrix(result)
     }
 
+    private fun rowScaling(row: Int, factor: Double) {
+        for (i in 0 until cols) this[row, i] *= factor
+    }
+
+    private fun Double.equalsDelta(other: Double) = abs(this - other) < max(Math.ulp(this), Math.ulp(other)) * 2
+
     companion object {
+
         fun of(vararg doubleArray: DoubleArray) = Matrix(doubleArray.asList().toTypedArray())
+
+        fun of(vararg intArray: IntArray): Matrix {
+            return Matrix(intArray.map { rows ->
+                rows.map { it.toDouble() }.toDoubleArray()
+            }.toTypedArray())
+        }
+
+        fun identity(size: Int): Matrix {
+            val value = Array(size) { DoubleArray(size) }
+            for (i in 0 until size) value[i][i] = 1.0
+            return Matrix(value)
+        }
     }
 }
--- test/MatrixTest.kt
+++ test/MatrixTest.kt
@@ -1,24 +1,19 @@
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 internal class MatrixTest {
 
-    private lateinit var matrix: Matrix
-
-    @BeforeEach
-    fun setupAll() {
-        matrix = Matrix.of(doubleArrayOf(1.0, 2.0, 3.0))
-    }
 
     @Test
     fun plusTest() {
+        var matrix = Matrix.of(doubleArrayOf(1.0, 2.0, 3.0))
         matrix += matrix
         assertEquals(Matrix.of(doubleArrayOf(2.0, 4.0, 6.0)), matrix)
     }
 
     @Test
     fun minusTest() {
+        var matrix = Matrix.of(doubleArrayOf(1.0, 2.0, 3.0))
         matrix -= matrix
         assertEquals(Matrix.of(doubleArrayOf(0.0, 0.0, 0.0)), matrix)
     }
@@ -36,9 +31,34 @@ internal class MatrixTest {
         )
         val C = A * B
         val expected = Matrix.of(
-            doubleArrayOf(22.0,28.0),
+            doubleArrayOf(22.0, 28.0),
             doubleArrayOf(49.0, 64.0)
         )
         assertEquals(expected, C)
+    }
+
+    @Test
+    fun detTest() {
+        val M = Matrix.of(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(3.0, 2.0, 1.0),
+            doubleArrayOf(1.0, 0.0, 1.0)
+        )
+        assertEquals(-8.0, M.det())
+    }
+
+    @Test
+    fun inverse() {
+        val M = Matrix.of(
+            intArrayOf(1, 0, 5),
+            intArrayOf(2, 1, 6),
+            intArrayOf(3, 4, 0)
+        )
+        val expected = Matrix.of(
+            intArrayOf(-24, 20, -5),
+            intArrayOf(18, -15, 4),
+            intArrayOf(5, -4, 1)
+        )
+        assertEquals(expected, M.inverse())
     }
 }
